ce6d6dcfcd9a7d0eaff331364ce7e6401b1db321
Kichou <kichouuuuu@gmail.com>
2026-06-11 14:01:42 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {
-  title: "WorkTracker",
+  title: "WorkProject",
   description: "Gestion de projets collaborative",
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
