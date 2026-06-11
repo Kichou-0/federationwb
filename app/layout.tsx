95892f4bd97ab48e8b1ca847c840da65abff1702
Kichou <kichouuuuu@gmail.com>
2026-06-11 14:18:43 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   title: "WorkProject",
   description: "Gestion de projets collaborative",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/app/favicon.ico",
   },
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
